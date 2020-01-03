8f36313e50d622ca080d6b6db3311eedd52f02da
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-01-03 23:46:04 +0100

    Fix release 1.5.0

--- arch/linux/build-deb.sh
+++ arch/linux/build-deb.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+PROJECT_NAME=cesium
+REPO="duniter/cesium"
+REPO_PUBLIC_URL="https://github.com/${REPO}"
+
 # NVM
 export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # Prepare
 NVER=`node -v`
-CESIUM_TAG=
+TAG=
 NW_VERSION=0.42.2
 NW_RELEASE="v${NW_VERSION}"
 NW_BASENAME=nwjs
@@ -24,13 +28,13 @@ mkdir -p "$DOWNLOADS"
 # -----------
 # Clean sources + releases
 # -----------
-rm -rf "$DOWNLOADS/cesium"
-rm -rf "$DOWNLOADS/cesium_src"
+rm -rf "$DOWNLOADS/${PROJECT_NAME}"
+rm -rf "$DOWNLOADS/${PROJECT_NAME}_src"
 rm -rf "$RELEASES"
 rm -rf /vagrant/*.deb
 rm -rf /vagrant/*.tar.gz
 
-mkdir -p $DOWNLOADS/cesium
+mkdir -p $DOWNLOADS/${PROJECT_NAME}
 
 # -----------
 # Downloads
@@ -38,29 +42,28 @@ mkdir -p $DOWNLOADS/cesium
 
 cd "$DOWNLOADS"
 
-if [[ ! -d "$DOWNLOADS/cesium_src" ]]; then
-  git clone https://github.com/duniter/cesium.git cesium_src
+if [ ! -d "$DOWNLOADS/${PROJECT_NAME}_src" ]; then
+  git clone ${REPO_PUBLIC_URL}.git ${PROJECT_NAME}_src
 fi
 
-cd cesium_src
+cd ${PROJECT_NAME}_src
 COMMIT=`git rev-list --tags --max-count=1`
-CESIUM_TAG=`echo $(git describe --tags $COMMIT) | sed 's/^v//'`
+$TAG=`echo $(git describe --tags $COMMIT) | sed 's/^v//'`
 cd ..
 
-CESIUM_RELEASE="cesium-v$CESIUM_TAG-web"
-echo "Checking that Cesium binary has been downloaded"
-if [[ ! -e "$DOWNLOADS/$CESIUM_RELEASE.zip" ]]; then
+ZIP_BASENAME="${PROJECT_NAME}-v$TAG-web"
+echo "Checking that ${PROJECT_NAME} binary has been downloaded"
+if [ ! -e "$DOWNLOADS/${ZIP_BASENAME}.zip" ]; then
     echo "Have to download it into ${DOWNLOADS}"
-    cd cesium
-
-    wget -kL "https://github.com/duniter/cesium/releases/download/v$CESIUM_TAG/$CESIUM_RELEASE.zip"
-    unzip $CESIUM_RELEASE.zip
-    rm $CESIUM_RELEASE.zip
+    cd ${PROJECT_NAME}
+    wget -kL "${REPO_PUBLIC_URL}/releases/download/v${TAG}/${ZIP_BASENAME}.zip"
+    unzip ${ZIP_BASENAME}.zip
+    rm ${ZIP_BASENAME}.zip
     cd ..
 fi
 
-CESIUM_DEB_VER=" $CESIUM_TAG"
-CESIUM_TAG="v$CESIUM_TAG"
+DEB_VER=" $TAG"
+TAG="v$TAG"
 
 # Get NW.js
 if [[ ! -d "$DOWNLOADS/$NW" ]]; then
@@ -77,11 +80,11 @@ fi
 rm -rf "$RELEASES"
 mkdir -p "$RELEASES"
 
-cp -r "$DOWNLOADS/cesium" "$RELEASES/cesium"
+cp -r "$DOWNLOADS/${PROJECT_NAME}" "$RELEASES/${PROJECT_NAME}"
 cd "$RELEASES"
 
 # Releases builds
-cd ${RELEASES}/cesium
+cd ${RELEASES}/${PROJECT_NAME}
 # Remove git files
 rm -Rf .git
 
@@ -97,15 +100,15 @@ mkdir -p "$RELEASES/desktop_release"
 # -------------------------------------------------
 
 cp -r "$DOWNLOADS/${NW}" "$RELEASES/desktop_release/nw"
-cp -r "$DOWNLOADS/cesium" "$RELEASES/desktop_release/nw/"
+cp -r "$DOWNLOADS/${PROJECT_NAME}" "$RELEASES/desktop_release/nw/"
 
 # Specific desktop files
 cp -r /vagrant/package.json "$RELEASES/desktop_release/nw/"
 cp -r /vagrant/yarn.lock "$RELEASES/desktop_release/nw/"
-cp -r /vagrant/node.js "$RELEASES/desktop_release/nw/cesium"
+cp -r /vagrant/node.js "$RELEASES/desktop_release/nw/${PROJECT_NAME}"
 # Injection
-sed -i 's/<script src="config.js"><\/script>/<script src="config.js"><\/script><script src="node.js"><\/script>/' "$RELEASES/desktop_release/nw/cesium/index.html" || exit 1
-sed -i 's/<script src="config.js"><\/script>/<script src="config.js"><\/script><script src="node.js"><\/script>/' "$RELEASES/desktop_release/nw/cesium/debug.html" || exit 1
+sed -i 's/<script src="config.js"><\/script>/<script src="config.js"><\/script><script src="node.js"><\/script>/' "$RELEASES/desktop_release/nw/${PROJECT_NAME}/index.html" || exit 1
+sed -i 's/<script src="config.js"><\/script>/<script src="config.js"><\/script><script src="node.js"><\/script>/' "$RELEASES/desktop_release/nw/${PROJECT_NAME}/debug.html" || exit 1
 
 # Specific desktop dependencies (for reading Duniter conf, ...)
 cd "$RELEASES/desktop_release/nw"
@@ -114,22 +117,22 @@ yarn
 # Releases
 cp -R "$RELEASES/desktop_release" "$RELEASES/desktop_release_tgz"
 cd "$RELEASES/desktop_release_tgz"
-tar czf /vagrant/cesium-desktop-${CESIUM_TAG}-linux-x64.tar.gz * --exclude ".git" --exclude "coverage" --exclude "test"
+tar czf /vagrant/${PROJECT_NAME}-desktop-${TAG}-linux-x64.tar.gz * --exclude ".git" --exclude "coverage" --exclude "test"
 
 # -------------------------------------------------
 # Build Desktop version .deb
 # -------------------------------------------------
 
 # Create .deb tree + package it
-cp -rf "/vagrant/package" "$RELEASES/cesium-x64" || exit 1
-mkdir -p "$RELEASES/cesium-x64/opt/cesium/"
-chmod 755 ${RELEASES}/cesium-x64/DEBIAN/post*
-chmod 755 ${RELEASES}/cesium-x64/DEBIAN/pre*
-sed -i "s/Version:.*/Version:$CESIUM_DEB_VER/g" ${RELEASES}/cesium-x64/DEBIAN/control || exit 1
+cp -r "/vagrant/package" "$RELEASES/${PROJECT_NAME}-x64" || exit 1
+mkdir -p "$RELEASES/${PROJECT_NAME}-x64/opt/${PROJECT_NAME}/" || exit 1
+chmod 755 ${RELEASES}/${PROJECT_NAME}-x64/DEBIAN/post*
+chmod 755 ${RELEASES}/${PROJECT_NAME}-x64/DEBIAN/pre*
+sed -i "s/Version:.*/Version:$DEB_VER/g" ${RELEASES}/${PROJECT_NAME}-x64/DEBIAN/control || exit 1
 cd ${RELEASES}/desktop_release/nw || exit 1
-zip -qr ${RELEASES}/cesium-x64/opt/cesium/nw.nwb *
+zip -qr ${RELEASES}/${PROJECT_NAME}-x64/opt/${PROJECT_NAME}/nw.nwb *
 
-sed -i "s/Package: .*/Package: cesium-desktop/g" ${RELEASES}/cesium-x64/DEBIAN/control || exit 1
+sed -i "s/Package: .*/Package: ${PROJECT_NAME}-desktop/g" ${RELEASES}/${PROJECT_NAME}-x64/DEBIAN/control || exit 1
 cd ${RELEASES}/ || exit 1
-fakeroot dpkg-deb --build cesium-x64 || exit 1
-mv cesium-x64.deb /vagrant/cesium-desktop-${CESIUM_TAG}-linux-x64.deb || exit 1
+fakeroot dpkg-deb --build ${PROJECT_NAME}-x64 || exit 1
+mv ${PROJECT_NAME}-x64.deb /vagrant/${PROJECT_NAME}-desktop-${TAG}-linux-x64.deb || exit 1
