6ba5336b0ce564d8bedcb42c4e0a99f73a56645c
Benoit Lavenier <benoit.lavenier@e-is.pro>
2024-02-24 12:35:02 +0100

    Release 1.7.13

--- arch/linux/user-bootstrap.sh
+++ arch/linux/user-bootstrap.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # Node.js
-nvm install 16
+nvm install 18
 
 # node-pre-gyp
 npm install -g nw-gyp node-pre-gyp
